3094ac97b07cac686568a3cf920f37088d0078e4
Douglas Souza <douglas_search@hotmail.com>
2020-10-14 22:29:22 -0300

    finalization of the front-end and communication with the back-end through the REST API

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,35 +1,32 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-
-import '../styles/global.css'
-import '../styles/pages/landing.css';
-
 import logoImg from '../images/logo.svg';
+import { Link } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi';
 
-function Landing() {
-    return (
-        <div id="page-landing">
-            <div className="content-wrapper">
-                <img src={logoImg} alt="Happy" />
+import '../styles/pages/landing.css'
 
-                <main>
-                    <h1>Leve felicidade para o mundo</h1>
-                    <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-                </main>
+const Landing: React.FC = () => {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="logo"/>
 
-                <div className="location">
-                    <strong>Jandira</strong>
-                    <span>São Paulo</span>
-                </div>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
 
-                <Link to="/app" className="enter-app">
-                    <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-                </Link>
-
-            </div>
+        <div className="location">
+          <strong>Jandira</strong>
+          <span>São Paulo</span>
         </div>
-    );
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
+        </Link>
+      </div>
+  </div>
+  )
 }
 
 export default Landing;
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,46 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
-
-import 'leaflet/dist/leaflet.css';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
 import '../styles/pages/orphanages-map.css';
 
-function OrphanagesMap() {
-    return (
-        <div id="page-map">
-            <aside>
-                <header>
-                    <img src={mapMarkerImg} alt="Happy" />
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
-                    <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças estão esperando a sua visita :)</p>
-                </header>
+const OrphanagesMap: React.FC = () => {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
 
-                <footer>
-                    <strong>Jandira</strong>
-                    <span>São Paulo</span>
-                </footer>
+  useEffect(() => {
+    api.get('/orphanages').then((response) => {
 
-            </aside>
+      setOrphanages(response.data)
+    });
+  }, []);
 
-            <Map
-                center={[-23.5389016, -46.912541]}
-                zoom={15}
-                style={{ width: '100%', height: '100%' }}
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Jandira</strong>
+          <span>São Paulo</span>
+        </footer>
+
+      </aside>
+
+      <Map
+        center={[-23.5389016, -46.912541]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+        {orphanages.map((orphanage) => (
+          <Marker
+            key={orphanage.id}
+            icon={mapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+          >
+            <Popup
+              closeButton={false}
+              minWidth={240}
+              maxWidth={240}
+              className="map-popup"
             >
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+              {orphanage.name}
+              <Link to={`/orphanages/${orphanage.id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </Link>
+            </Popup>
+          </Marker>
+        ))}
 
-            </Map>
+      </Map>
 
-            <Link to="" className="create-orphanages">
-                <FiPlus size={32} color="#FFF" />
-            </Link>
-        </div>
-    )
+      <Link to="/orphanages/create" className="create-orphanage">
+        <FiPlus size="32" color="#fff" />
+      </Link>
+    </div>
+  )
 }
 
 export default OrphanagesMap;
